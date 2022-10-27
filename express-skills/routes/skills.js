4d7c8fc01d2c9d022c0eed73ea491f0419c181f9
dunndealpro <dunn.evan.f@gmail.com>
2022-10-27 12:49:29 -0700

    add delete button

--- express-skills/routes/skills.js
+++ express-skills/routes/skills.js
@@ -13,6 +13,9 @@ var skillsCtrl = require('../controllers/skills');
 // GET /skills
 router.get('/', skillsCtrl.index);
 // GET /skills/:id
-router.get('/:id', skillsCtrl.show);
+router.get('/new', skillsCtrl.new)
+router.get('/:id', skillsCtrl.show)
+router.post('/', skillsCtrl.create)
+router.delete('/:id', skillsCtrl.delete)
 
 module.exports = router;
